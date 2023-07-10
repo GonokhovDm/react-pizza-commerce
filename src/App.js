037393bcf8829b1e5a697525c5549cf92a9ad427
Dmitry Gonokhov <gonokhov.dm@gmail.com>
2023-07-10 16:11:25 +0700

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import './scss/app.scss';
+import { Routes, Route } from "react-router-dom";
 
 import Header from './components/Header'
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock'
+import Home from './pages/Home'
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+
 import React from 'react';
 
 function App() {
-
-  const [items, setItems] = React.useState([]);
-
-  fetch('https://64a584da00c3559aa9bfd62c.mockapi.io/items')
-    .then((res) => {
-      return res.json();
-    })
-    .then ((arr) => {
-      setItems(arr);
-    })
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map(obj =>(
-              <PizzaBlock key={obj.id} {...obj}/>
-            ))}
-          </div>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
